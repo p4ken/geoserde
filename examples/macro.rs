ea7a75f9f1a2c490547dd14fea89bb69a9e0bb35
p4ken <commit@p4ken.skr.jp>
2024-10-14 19:13:51 +0900

    proposal: with

--- examples/macro.rs
+++ examples/macro.rs
@@ -5,9 +5,24 @@ fn main() {
     let feat = MyFeature::default();
 }
 
-#[derive(Default, Serialize, Geoserde)]
+use geoserde::geometry;
+
+#[derive(Default, Serialize)]
 struct MyFeature {
-    #[geoserde(geometry)]
+    #[serde(with = "geometry")]
     loc: Point,
     title: String,
+}
+
+pub mod geoserde {
+    pub use geometry::serialize;
+
+    pub mod geometry {
+        pub fn serialize<T, S>(t: &T, s: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer,
+        {
+            todo!()
+        }
+    }
 }
