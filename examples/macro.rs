274652a77622ae330540e87314688c9e77d0efe4
p4ken <commit@p4ken.skr.jp>
2024-11-09 21:21:41 +0900

    Add attribute macro

--- examples/macro.rs
+++ examples/macro.rs
@@ -1,28 +1,27 @@
 use geo_types::Point;
 use serde::Serialize;
 
-fn main() {
-    let feat = MyFeature::default();
+fn main() {}
+
+pub mod geoserde {
+    pub mod geometry {
+        pub fn serialize<T, S: serde::Serializer>(_t: &T, _s: S) -> Result<S::Ok, S::Error> {
+            todo!()
+        }
+    }
 }
 
-use geoserde::geometry;
-
 #[derive(Default, Serialize)]
-struct MyFeature {
-    #[serde(with = "geometry")]
+struct MyFeature1 {
+    #[serde(with = "geoserde::geometry")]
     loc: Point,
     title: String,
 }
 
-pub mod geoserde {
-    pub use geometry::serialize;
-
-    pub mod geometry {
-        pub fn serialize<T, S>(t: &T, s: S) -> Result<S::Ok, S::Error>
-        where
-            S: serde::Serializer,
-        {
-            todo!()
-        }
-    }
+#[derive(Default, Serialize)]
+struct MyFeature2 {
+    // 技術的にこれだけで可能なのか？
+    #[geoserde_(geometry)]
+    loc: Point,
+    title: String,
 }
---
+++ macros/src/lib.rs
@@ -0,0 +1,2 @@
+#[proc_macro_attribute]
+pub fn geoserde_(attr: TokenStream, item: TokenStream) -> TokenStream {}
