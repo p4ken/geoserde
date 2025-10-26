c5b6aa09cca939bf5f8bec89d1e5b01b4ffe27a3
p4ken <commit@p4ken.skr.jp>
2025-10-26 20:47:39 +0900

    [skip ci] WIP

--- tests/nested.rs
+++ tests/nested.rs
@@ -1,17 +1,27 @@
 // TODO: ネストしたクラス。geojsonはany objectだから明示的にflattenが必要。
 
 use geo_types::LineString;
-use geoserde::GeoDeserialize;
+use geoserde::{DeserializeFeature, GeoDeserialize};
 use serde::Deserialize;
 
-#[derive(GeoDeserialize)]
 struct Parent {
-    #[flatten]
     wrapped_geometry: Child,
+    property: i32,
 }
+impl DeserializeFeature for Parent {
+    fn deserialize_feature(fmt: impl geoserde::ParseFeature) -> Self {
+        let wrapped_geometry = Child::deserialize_feature(fmt);
+        let ((), p) = fmt.parse_feature();
+        Self {
+            wrapped_geometry,
+            property: p,
+        }
+    }
+}
+
 
 #[derive(GeoDeserialize)]
 struct Child {
-    #[geometry]
     shape: LineString,
+    property: bool,
 }
