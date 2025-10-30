5ea176c2ad1faa128911a20c5c708017b9bb4459
p4ken <commit@p4ken.skr.jp>
2025-10-30 19:43:35 +0900

    Renewal serde_with concept

--- prototype-v0_6/tests/2-serde_with.rs
+++ prototype-v0_6/tests/2-serde_with.rs
@@ -1,32 +1,91 @@
-use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
+
+use serde::{
+    de::{DeserializeOwned, MapAccess, Visitor},
+    Deserialize, Deserializer, Serialize,
+};
 
 #[derive(Serialize, Deserialize)]
-pub struct Child1 {
+struct Child1 {
     #[serde(with = "geometry")]
     loc: geo_types::Point,
     count: i32,
 }
 
 #[derive(Serialize)]
-pub struct MyFeature1 {
+struct MyFeature1 {
     child: Child1,
     title: String,
 }
 
 pub mod geometry {
+    use geo_traits::{CoordTrait, PointTrait};
+    use serde::{ser::SerializeStruct, Deserialize, Serialize};
+
     pub fn serialize<S: serde::Serializer>(
-        point: &impl Into<geo_types::Geometry>,
+        geom: impl super::SerializeGeometry,
         ser: S,
     ) -> Result<S::Ok, S::Error> {
-        // 普通の構造体名と被らない名前
-        let geom: geo_types::Geometry = point.into();
-        ser.serialize_newtype_struct("__geoserde_geometry", geom)
+        super::__GeoSerdeGeometry(geom).serialize(ser)
     }
-    pub fn deserialize<'a, D: serde::Deserializer<'a>, G>(_de: D) -> Result<G, D::Error> {
-        // de -> reader // これができない
-        // reader -> LineString etc.
-        todo!()
+
+    // pub fn serialize0<S: serde::Serializer>(
+    //     source: impl geo_traits::GeometryTrait<T: Serialize>,
+    //     ser: S,
+    // ) -> Result<S::Ok, S::Error> {
+    //     match source.as_type() {
+    //         geo_traits::GeometryType::Point(point) => {
+    //             // 普通の構造体名と被らないname
+    //             let mut point_ser = ser.serialize_struct("geoserde::Point", 2).unwrap();
+    //             let coord = point.coord().unwrap();
+    //             point_ser
+    //                 .serialize_field("geoserde::x", &coord.x())
+    //                 .unwrap();
+    //             point_ser
+    //                 .serialize_field("geoserde::y", &coord.y())
+    //                 .unwrap();
+    //             point_ser.end()
+    //         }
+    //         _ => todo!(),
+    //     }
+    // }
+    pub fn deserialize<'a, D: serde::Deserializer<'a>, G: super::DeserializeGeometry>(
+        de: D,
+    ) -> Result<G, D::Error> {
+        Ok(super::__GeoSerdeGeometry::deserialize(de)?.0)
     }
 }
+
+pub trait SerializeGeometry : Serialize {}
+impl SerializeGeometry for geo_types::Point {}
+impl<T: SerializeGeometry> SerializeGeometry for &T {}
+
+// TODO: sealed
+pub trait DeserializeGeometry : DeserializeOwned {}
+impl DeserializeGeometry for geo_types::Point {}
+
+// Wrapper to tell "this is the geometry" for data formats.
+#[derive(Serialize, Deserialize)]
+struct __GeoSerdeGeometry<T>(T);
+
+// pub trait DeserializeGeometry0: Sized {
+//     fn deserialize_geometry<'a, D: serde::Deserializer<'a>>(de: D) -> Result<Self, D::Error>;
+// }
+// impl DeserializeGeometry0 for geo_types::Point {
+//     fn deserialize_geometry<'a, D: serde::Deserializer<'a>>(de: D) -> Result<Self, D::Error> {
+//         // The name prevents collisions with real structs.
+//         de.deserialize_newtype_struct("geoserde::Geometry", GeometryVisitor::default())
+//     }
+// }
+
+// #[derive(Default)]
+// struct GeometryVisitor<T>(PhantomData<T>);
+// impl<'de, T> Visitor<'de> for GeometryVisitor<T> {
+//     type Value = T;
+
+//     fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+//         todo!()
+//     }
+// }
 
 fn main() {}
--- tests/v0_6/nested.rs
+++ tests/v0_6/nested.rs
@@ -5,13 +5,17 @@ use geoserde::{DeserializeFeature, GeoDeserialize};
 use serde::Deserialize;
 
 struct Parent {
+    // #[geometry]
     wrapped_geometry: Child,
     property: i32,
 }
 impl DeserializeFeature for Parent {
     fn deserialize_feature(fmt: impl geoserde::ParseFeature) -> Self {
         let wrapped_geometry = Child::deserialize_feature(fmt);
-        todo!();
+        // ここのエラーは致命的。
+        // serde の flatten のようなことをするには、
+        // かなり大掛かりで serde そっくりの処理が必要になる。
+        // いっそのこと serde に寄せて serde(with) などで解決できないか考えることに。
         let ((), p) = fmt.parse_feature();
         Self {
             wrapped_geometry,
@@ -22,6 +26,7 @@ impl DeserializeFeature for Parent {
 
 #[derive(GeoDeserialize)]
 struct Child {
+    #[geometry]
     shape: LineString,
     property: bool,
 }
