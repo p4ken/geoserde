5c45384b47cbbf793b8d2b09b4018b5c4ad87763
p4ken <commit@p4ken.skr.jp>
2025-10-30 19:49:17 +0900

    Add v0_6_1

--- prototype-v0_6/tests/2-serde_with.rs
+++ prototype-v0_6/tests/2-serde_with.rs
@@ -28,27 +28,6 @@ pub mod geometry {
     ) -> Result<S::Ok, S::Error> {
         super::__GeoSerdeGeometry(geom).serialize(ser)
     }
-
-    // pub fn serialize0<S: serde::Serializer>(
-    //     source: impl geo_traits::GeometryTrait<T: Serialize>,
-    //     ser: S,
-    // ) -> Result<S::Ok, S::Error> {
-    //     match source.as_type() {
-    //         geo_traits::GeometryType::Point(point) => {
-    //             // 普通の構造体名と被らないname
-    //             let mut point_ser = ser.serialize_struct("geoserde::Point", 2).unwrap();
-    //             let coord = point.coord().unwrap();
-    //             point_ser
-    //                 .serialize_field("geoserde::x", &coord.x())
-    //                 .unwrap();
-    //             point_ser
-    //                 .serialize_field("geoserde::y", &coord.y())
-    //                 .unwrap();
-    //             point_ser.end()
-    //         }
-    //         _ => todo!(),
-    //     }
-    // }
     pub fn deserialize<'a, D: serde::Deserializer<'a>, G: super::DeserializeGeometry>(
         de: D,
     ) -> Result<G, D::Error> {
@@ -67,25 +46,5 @@ impl DeserializeGeometry for geo_types::Point {}
 // Wrapper to tell "this is the geometry" for data formats.
 #[derive(Serialize, Deserialize)]
 struct __GeoSerdeGeometry<T>(T);
-
-// pub trait DeserializeGeometry0: Sized {
-//     fn deserialize_geometry<'a, D: serde::Deserializer<'a>>(de: D) -> Result<Self, D::Error>;
-// }
-// impl DeserializeGeometry0 for geo_types::Point {
-//     fn deserialize_geometry<'a, D: serde::Deserializer<'a>>(de: D) -> Result<Self, D::Error> {
-//         // The name prevents collisions with real structs.
-//         de.deserialize_newtype_struct("geoserde::Geometry", GeometryVisitor::default())
-//     }
-// }
-
-// #[derive(Default)]
-// struct GeometryVisitor<T>(PhantomData<T>);
-// impl<'de, T> Visitor<'de> for GeometryVisitor<T> {
-//     type Value = T;
-
-//     fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-//         todo!()
-//     }
-// }
 
 fn main() {}
