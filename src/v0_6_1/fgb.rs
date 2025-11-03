ea6dc805d068cc54915b520f44bcdc0b4f7e203c
p4ken <commit@p4ken.skr.jp>
2025-11-03 21:27:13 +0900

    Deserialize i32

--- src/v0_6_1/fgb.rs
+++ src/v0_6_1/fgb.rs
@@ -1,8 +1,34 @@
-use flatgeobuf::FgbFeature;
-use serde::Deserializer;
+use flatgeobuf::{ColumnType, FgbFeature};
+use serde::{
+    de::{value::StrDeserializer, MapAccess},
+    Deserializer,
+};
 
-pub struct FeatureDeserializer<'de>(pub &'de FgbFeature);
-impl<'de> Deserializer<'de> for FeatureDeserializer<'de> {
+// pub struct LayerDeserializer {}
+// impl LayerDeserializer {
+//     pub fn new<R, S>(layer: FeatureIter<R, S>) -> Self {
+//         Self {}
+//     }
+// }
+
+pub struct FeatureDeserializer<'de> {
+    cols: &'de [(String, ColumnType)],
+    col_type: Option<ColumnType>,
+    properties_buf: &'de [u8],
+}
+impl<'de> FeatureDeserializer<'de> {
+    pub fn new(cols: &'de [(String, ColumnType)], feat: &'de FgbFeature) -> Self {
+        Self {
+            cols,
+            col_type: None,
+            properties_buf: match feat.fbs_feature().properties() {
+                Some(fbs) => fbs.bytes(),
+                None => &[],
+            },
+        }
+    }
+}
+impl<'de, 'a> Deserializer<'de> for &'a mut FeatureDeserializer<'de> {
     type Error = serde::de::value::Error; // TODO
 
     fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
@@ -37,7 +63,16 @@ impl<'de> Deserializer<'de> for FeatureDeserializer<'de> {
     where
         V: serde::de::Visitor<'de>,
     {
-        todo!()
+        match self.col_type {
+            Some(ColumnType::Int) => visitor.visit_i32(i32::from_le_bytes(
+                self.properties_buf
+                    .split_off(..4)
+                    .expect("i32 requires 4 bits")
+                    .try_into()
+                    .unwrap(),
+            )),
+            _ => todo!(),
+        }
     }
 
     fn deserialize_i64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
@@ -205,7 +240,7 @@ impl<'de> Deserializer<'de> for FeatureDeserializer<'de> {
     where
         V: serde::de::Visitor<'de>,
     {
-        todo!()
+        visitor.visit_map(self)
     }
 
     fn deserialize_enum<V>(
@@ -232,5 +267,185 @@ impl<'de> Deserializer<'de> for FeatureDeserializer<'de> {
         V: serde::de::Visitor<'de>,
     {
         todo!()
+    }
+}
+
+// struct PropertyAccess<'a, 'de: 'a> {
+//     feat: &'a FeatureDeserializer<'de>,
+//     col_idx: usize,
+// }
+impl<'de> MapAccess<'de> for FeatureDeserializer<'de> {
+    type Error = serde::de::value::Error; // tmp
+
+    fn next_key_seed<K>(&mut self, seed: K) -> Result<Option<K::Value>, Self::Error>
+    where
+        K: serde::de::DeserializeSeed<'de>,
+    {
+        let col_index = match self.properties_buf.split_off(..2) {
+            Some(bin) => u16::from_le_bytes(bin.try_into().unwrap()) as usize,
+            None => return Ok(None),
+        };
+        let (col_name, col_type) = match self.cols.get(col_index) {
+            Some(col) => col,
+            None => return Ok(None),
+        };
+        let value = seed.deserialize(StrDeserializer::new(col_name))?;
+        self.col_type = Some(*col_type);
+        Ok(Some(value))
+    }
+
+    fn next_value_seed<V>(&mut self, seed: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::DeserializeSeed<'de>,
+    {
+        seed.deserialize(self)
+
+        // let column = &columns_meta.get(column_idx);
+        // match column.type_() {
+        //     ColumnType::Int => {
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::Int(LittleEndian::read_i32(&bytes[offset..offset + 4])),
+        //         )?;
+        //         offset += size_of::<i32>();
+        //     }
+        //     ColumnType::Long => {
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::Long(LittleEndian::read_i64(&bytes[offset..offset + 8])),
+        //         )?;
+        //         offset += size_of::<i64>();
+        //     }
+        //     ColumnType::ULong => {
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::ULong(LittleEndian::read_u64(&bytes[offset..offset + 8])),
+        //         )?;
+        //         offset += size_of::<u64>();
+        //     }
+        //     ColumnType::Double => {
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::Double(LittleEndian::read_f64(&bytes[offset..offset + 8])),
+        //         )?;
+        //         offset += size_of::<f64>();
+        //     }
+        //     ColumnType::String => {
+        //         let len = LittleEndian::read_u32(&bytes[offset..offset + 4]) as usize;
+        //         offset += size_of::<u32>();
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::String(
+        //                 // unsafe variant without UTF-8 checking would be faster...
+        //                 str::from_utf8(&bytes[offset..offset + len]).map_err(|_| {
+        //                     GeozeroError::Property("Invalid UTF-8 encoding".to_string())
+        //                 })?,
+        //             ),
+        //         )?;
+        //         offset += len;
+        //     }
+        //     ColumnType::Byte => {
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::Byte(bytes[offset] as i8),
+        //         )?;
+        //         offset += size_of::<i8>();
+        //     }
+        //     ColumnType::UByte => {
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::UByte(bytes[offset]),
+        //         )?;
+        //         offset += size_of::<u8>();
+        //     }
+        //     ColumnType::Bool => {
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::Bool(bytes[offset] != 0),
+        //         )?;
+        //         offset += size_of::<u8>();
+        //     }
+        //     ColumnType::Short => {
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::Short(LittleEndian::read_i16(&bytes[offset..offset + 2])),
+        //         )?;
+        //         offset += size_of::<i16>();
+        //     }
+        //     ColumnType::UShort => {
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::UShort(LittleEndian::read_u16(&bytes[offset..offset + 2])),
+        //         )?;
+        //         offset += size_of::<u16>();
+        //     }
+        //     ColumnType::UInt => {
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::UInt(LittleEndian::read_u32(&bytes[offset..offset + 4])),
+        //         )?;
+        //         offset += size_of::<u32>();
+        //     }
+        //     ColumnType::Float => {
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::Float(LittleEndian::read_f32(&bytes[offset..offset + 4])),
+        //         )?;
+        //         offset += size_of::<f32>();
+        //     }
+        //     ColumnType::Json => {
+        //         let len = LittleEndian::read_u32(&bytes[offset..offset + 4]) as usize;
+        //         offset += size_of::<u32>();
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::Json(
+        //                 // JSON may be represented using UTF-8, UTF-16, or UTF-32. The default encoding is UTF-8.
+        //                 str::from_utf8(&bytes[offset..offset + len]).map_err(|_| {
+        //                     GeozeroError::Property("Invalid UTF-8 encoding".to_string())
+        //                 })?,
+        //             ),
+        //         )?;
+        //         offset += len;
+        //     }
+        //     ColumnType::DateTime => {
+        //         let len = LittleEndian::read_u32(&bytes[offset..offset + 4]) as usize;
+        //         offset += size_of::<u32>();
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::DateTime(
+        //                 // unsafe variant without UTF-8 checking would be faster...
+        //                 str::from_utf8(&bytes[offset..offset + len]).map_err(|_| {
+        //                     GeozeroError::Property("Invalid UTF-8 encoding".to_string())
+        //                 })?,
+        //             ),
+        //         )?;
+        //         offset += len;
+        //     }
+        //     ColumnType::Binary => {
+        //         let len = LittleEndian::read_u32(&bytes[offset..offset + 4]) as usize;
+        //         offset += size_of::<u32>();
+        //         finish = reader.property(
+        //             column_idx,
+        //             column.name(),
+        //             &ColumnValue::Binary(&bytes[offset..offset + len]),
+        //         )?;
+        //         offset += len;
+        //     }
+        //     ColumnType(_) => {}
+        // }
     }
 }
--- tests/v0_6_1/flatgeobuf.rs
+++ tests/v0_6_1/flatgeobuf.rs
@@ -1,7 +1,7 @@
 use std::io::Cursor;
 
 use anyhow::Result;
-use flatgeobuf::{FallibleStreamingIterator, FgbFeature, FgbWriter, GeometryType};
+use flatgeobuf::{FallibleStreamingIterator, FgbWriter, FgbWriterOptions, GeometryType};
 use geo_traits::to_geo::ToGeoGeometry;
 use geoserde::v0_6_1::fgb::FeatureDeserializer;
 use geozero::{ColumnValue, FeatureProcessor, GeozeroGeometry, PropertyProcessor};
@@ -18,30 +18,44 @@ struct MyFeature {
 
 #[test]
 fn de() -> Result<()> {
-    let fgb_r = flatgeobuf::FgbReader::open(Cursor::new(fgb_buf()?))?;
-    let mut fgb_iter = fgb_r.select_all()?;
+    let mut fgb_iter = flatgeobuf::FgbReader::open(Cursor::new(fgb_buf()?))?.select_all()?;
+    // FgbFeature itself should implement Deserialize because it has header in private field
+    let fgb_header = fgb_iter
+        .header()
+        .columns()
+        .unwrap()
+        .iter()
+        .map(|col| (col.name().to_owned(), col.type_()))
+        .collect::<Vec<_>>();
     let mut my_features = vec![];
     while let Some(fgb_feat) = fgb_iter.next()? {
-        let my_feat = MyFeature::deserialize(FeatureDeserializer(fgb_feat));
+        let my_feat = MyFeature::deserialize(&mut FeatureDeserializer::new(&fgb_header, fgb_feat))?;
         my_features.push(my_feat);
     }
+    assert_eq!(my_features[0].number, 1);
+    // assert_eq!(my_features[1].number, 2);
     Ok(())
 }
 
 // Create in-memory flatgeobuf
 fn fgb_buf() -> Result<Vec<u8>> {
     let mut fgb_buf = vec![];
-    let mut fgb_w = FgbWriter::create("my_features", GeometryType::LineString)?;
+    let fgb_w_opt = FgbWriterOptions {
+        write_index: false,
+        ..Default::default()
+    };
+    let mut fgb_w =
+        FgbWriter::create_with_options("my_features", GeometryType::LineString, fgb_w_opt)?;
 
     testing::line(0).to_geometry().process_geom(&mut fgb_w)?;
     fgb_w.property(0, "number", &ColumnValue::Int(1))?;
-    fgb_w.property(1, "text", &ColumnValue::String("one"))?;
+    // fgb_w.property(1, "text", &ColumnValue::String("one"))?;
     fgb_w.feature_end(0)?;
 
-    testing::line(1).to_geometry().process_geom(&mut fgb_w)?;
-    fgb_w.property(0, "number", &ColumnValue::Int(2))?;
-    fgb_w.property(1, "text", &ColumnValue::String("two"))?;
-    fgb_w.feature_end(1)?;
+    // testing::line(1).to_geometry().process_geom(&mut fgb_w)?;
+    // fgb_w.property(1, "number", &ColumnValue::Int(2))?;
+    // // fgb_w.property(1, "text", &ColumnValue::String("two"))?;
+    // fgb_w.feature_end(1)?;
 
     fgb_w.write(&mut fgb_buf)?;
     Ok(fgb_buf)
