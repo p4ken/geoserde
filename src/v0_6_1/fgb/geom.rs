665fc469124836c39a6828226da76497f838ab3f
p4ken <commit@p4ken.skr.jp>
2026-01-18 19:57:23 +0900

    Refactor redundant de impls

--- src/v0_6_1/fgb/geom.rs
+++ src/v0_6_1/fgb/geom.rs
@@ -6,32 +6,6 @@ pub struct GeometryDeserializer<'de> {
     geom: flatgeobuf::Geometry<'de>,
 }
 
-// リングの範囲を持つラッパー
-struct RingGeometry<'de> {
-    geom: flatgeobuf::Geometry<'de>,
-    start: usize,
-    end: usize,
-}
-
-// LineStringをラップするための構造体
-struct LineStringWrapper<'de> {
-    geom: flatgeobuf::Geometry<'de>,
-    start: usize,
-    end: usize,
-}
-
-impl<'de> IntoDeserializer<'de, serde::de::value::Error> for LineStringWrapper<'de> {
-    type Deserializer = LineStringDeserializer<'de>;
-
-    fn into_deserializer(self) -> Self::Deserializer {
-        LineStringDeserializer {
-            geom: self.geom,
-            start: self.start,
-            end: self.end,
-        }
-    }
-}
-
 // LineStringのデシリアライザ
 struct LineStringDeserializer<'de> {
     geom: flatgeobuf::Geometry<'de>,
@@ -72,6 +46,14 @@ impl<'de> serde::Deserializer<'de> for LineStringDeserializer<'de> {
     }
 }
 
+impl<'de> IntoDeserializer<'de> for LineStringDeserializer<'de> {
+    type Deserializer = Self;
+
+    fn into_deserializer(self) -> Self {
+        self
+    }
+}
+
 impl<'de> GeometryDeserializer<'de> {
     pub fn new(geom: flatgeobuf::Geometry<'de>) -> Self {
         Self { geom }
@@ -86,24 +68,6 @@ impl<'de> GeometryDeserializer<'de> {
             None => Err(Error::custom("fbs has no coords")),
         }
     }
-
-    fn deserialize_point_seq<V>(&self, visitor: V) -> Result<V::Value, serde::de::value::Error>
-    where
-        V: serde::de::Visitor<'de>,
-    {
-        let de = SeqDeserializer::new(PointIter::new(self.geom));
-        visitor.visit_seq(de)
-    }
-
-    fn deserializable_polygon<V>(&self, visitor: V) -> Result<V::Value, serde::de::value::Error>
-    where
-        V: serde::de::Visitor<'de>,
-    {
-        let de = SeqDeserializer::new(std::iter::once(SeqDeserializer::new(PointIter::new(
-            self.geom,
-        ))));
-        visitor.visit_seq(de)
-    }
 }
 
 impl<'de> serde::Deserializer<'de> for GeometryDeserializer<'de> {
@@ -111,86 +75,6 @@ impl<'de> serde::Deserializer<'de> for GeometryDeserializer<'de> {
 
     fn deserialize_any<V: Visitor<'de>>(self, _: V) -> Result<V::Value, Self::Error> {
         Err(Error::custom("expected geometry type"))
-    }
-
-    fn deserialize_bool<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_i8<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_i16<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_i32<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_i64<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_u8<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_u16<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_u32<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_u64<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_f32<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_f64<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_char<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_str<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_string<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_bytes<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_byte_buf<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_option<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_unit<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_unit_struct<V: Visitor<'de>>(
-        self,
-        _name: &'static str,
-        visitor: V,
-    ) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
     }
 
     fn deserialize_newtype_struct<V: Visitor<'de>>(
@@ -212,7 +96,7 @@ impl<'de> serde::Deserializer<'de> for GeometryDeserializer<'de> {
                     let mut start = 0;
 
                     for end in ends {
-                        rings.push(LineStringWrapper {
+                        rings.push(LineStringDeserializer {
                             geom: self.geom,
                             start,
                             end: end as usize,
@@ -225,38 +109,8 @@ impl<'de> serde::Deserializer<'de> for GeometryDeserializer<'de> {
                     Err(Error::custom("Polygon has no ends"))
                 }
             }
-            _ => {
-                return Err(Error::invalid_type(
-                    serde::de::Unexpected::Other(name),
-                    &"geoserde::LineString or geoserde::Polygon",
-                ))
-            }
+            _ => self.deserialize_any(visitor),
         }
-    }
-
-    fn deserialize_seq<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_tuple<V: Visitor<'de>>(
-        self,
-        _len: usize,
-        visitor: V,
-    ) -> Result<V::Value, Self::Error> {
-        self.deserialize_seq(visitor)
-    }
-
-    fn deserialize_tuple_struct<V: Visitor<'de>>(
-        self,
-        _name: &'static str,
-        _len: usize,
-        visitor: V,
-    ) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_map<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
     }
 
     fn deserialize_struct<V: Visitor<'de>>(
@@ -267,24 +121,17 @@ impl<'de> serde::Deserializer<'de> for GeometryDeserializer<'de> {
     ) -> Result<V::Value, Self::Error> {
         match name {
             "geoserde::Point" => self.deserilize_point(visitor),
-            _ => todo!("{}", name),
+            _ => self.deserialize_any(visitor),
         }
-    }
-
-    fn deserialize_enum<V: Visitor<'de>>(
-        self,
-        _name: &'static str,
-        _variants: &'static [&'static str],
-        visitor: V,
-    ) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
-    }
-
-    fn deserialize_identifier<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
-        self.deserialize_any(visitor)
     }
 
     fn deserialize_ignored_any<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value, Self::Error> {
         visitor.visit_unit()
+    }
+
+    serde::forward_to_deserialize_any! {
+        bool i8 i16 i32 i64 u8 u16 u32 u64 f32 f64 char str string
+        bytes byte_buf option unit unit_struct seq tuple
+        tuple_struct map enum identifier
     }
 }
