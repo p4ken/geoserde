b2f761b89b7dbb5a0b9b2db8aa4f757f0c2bbe4b
p4ken <commit@p4ken.skr.jp>
2026-01-21 21:58:33 +0900

    AI: Impl LineStringIter

--- src/v0_6_1/fgb/iter.rs
+++ src/v0_6_1/fgb/iter.rs
@@ -5,22 +5,37 @@ use crate::v0_6_1::Point;
 pub struct PointIter<'a> {
     fbs: flatgeobuf::Geometry<'a>,
     index: usize,
+    len: usize,
 }
 
 impl<'a> PointIter<'a> {
     pub fn new(fbs: flatgeobuf::Geometry<'a>) -> Self {
-        Self { fbs, index: 0 }
+        let len = fbs.xy().map(|xy| xy.len() / 2).unwrap_or(0);
+        Self { fbs, index: 0, len }
+    }
+
+    pub fn with_range(fbs: flatgeobuf::Geometry<'a>, start: usize, end: usize) -> Self {
+        Self {
+            fbs,
+            index: start,
+            len: end,
+        }
     }
 }
 
 impl PointIter<'_> {
     fn xy(&self) -> Option<[f64; 2]> {
+        if self.index >= self.len {
+            return None;
+        }
         let mut iter = self.fbs.xy()?.iter().skip(self.index * 2);
         Some([iter.next()?, iter.next()?])
     }
+
     fn z(&self) -> Option<f64> {
         self.fbs.z()?.iter().nth(self.index)
     }
+
     fn m(&self) -> Option<f64> {
         self.fbs.m()?.iter().nth(self.index)
     }
@@ -44,10 +59,7 @@ impl Iterator for PointIter<'_> {
 
 impl ExactSizeIterator for PointIter<'_> {
     fn len(&self) -> usize {
-        match self.fbs.xy() {
-            Some(xy) => xy.len() / 2,
-            None => 0,
-        }
+        self.len.saturating_sub(self.index)
     }
 }
 
@@ -59,6 +71,32 @@ impl IntoDeserializer<'_> for PointIter<'_> {
     }
 }
 
-// struct MultiPart {}
+struct LineStringIter<'a> {
+    fbs: flatgeobuf::Geometry<'a>,
+    index: usize,
+}
 
-// struct GeometryCollection {}
+impl<'a> Iterator for LineStringIter<'a> {
+    type Item = PointIter<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let ends = self.fbs.ends()?;
+
+        if self.index >= ends.len() {
+            return None;
+        }
+
+        let start = if self.index == 0 {
+            0
+        } else {
+            ends.get(self.index - 1) as usize
+        };
+        let end = ends.get(self.index) as usize;
+
+        self.index += 1;
+
+        Some(PointIter::with_range(self.fbs, start, end))
+    }
+}
+
+struct PolygonIter {}
