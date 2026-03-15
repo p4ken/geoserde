c3ca22522c622162b61372e24764576d4262b7e8
p4ken <commit@p4ken.skr.jp>
2026-03-15 21:39:18 +0900

    Prototype nested macro

--- tests/v0_6_2/macro_feat.rs
+++ tests/v0_6_2/macro_feat.rs
@@ -1,39 +1,39 @@
 use std::borrow::Cow;
 
-use geoserde::v0_6_1::ser::prop::SerializeProperties;
+use geo_traits::{GeometryTrait, UnimplementedPoint};
+use geoserde::v0_6_2::fgb;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 struct Feat {
     name: Cow<'static, str>,
-    #[serde(skip, default = "geo_types::LineString::empty", rename = "geometry")]
-    shape: geo_types::LineString,
-    value: i32,
+    child: Child,
 }
 
 impl Feat {
-    fn serialize(
-        &self,
-        ser: &mut geoserde::v0_6_2::fgb::FeatureSerializer,
-    ) -> Result<(), geoserde::v0_6_2::fgb::ser::Error> {
-        #[derive(Serialize)]
-        struct Prop<'a> {
-            name: &'a Cow<'static, str>,
-            value: &'a i32,
-        }
-        let prop = Prop {
-            name: &self.name,
-            value: &self.value,
-        };
-        ser.serialize_feature(&self.shape, prop)
+    fn serialize(&self, ser: &mut fgb::FeatureSerializer) -> Result<(), fgb::ser::Error> {
+        // ser.serialize_feature(&self.child, &self)
+        todo!()
     }
 }
 
+#[derive(Serialize, Deserialize)]
+struct Child {
+    value: i32,
+    #[serde(skip, default = "geo_types::LineString::empty", rename = "geometry")]
+    shape: geo_types::LineString,
+}
+
+// impl<'a> GeometryTrait for &'a Child {}
+
 #[test]
+#[ignore = "WIP"]
 fn ser_test() {
     let feat = Feat {
         name: "Hello".into(),
-        shape: crate::testing::ls(0),
-        value: 42,
+        child: Child {
+            shape: crate::testing::ls(0),
+            value: 42,
+        },
     };
 }
